4ddb1ba63ebe449fe7382790791ee71191cf7981
Acryval <wojholf.98@gmail.com>
2022-05-15 00:48:05 +0200

    last commit?

--- src/Lexer.java
+++ src/Lexer.java
@@ -2,9 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lexer {
+    public static HashMap<String, TokenType> keywords = new HashMap<>();
+    public static Pattern LITERAL_INT_PATTERN = Pattern.compile("^-?[$%&]?\\d+");
+    public static Pattern LITERAL_REAL_PATTERN = Pattern.compile("^-?\\d+(((\\.\\d+)?e-?\\d+)|(\\.\\d+))");
+    public static Pattern IDENTIFIER_PATTERN = Pattern.compile("^\\D[^()\\[\\].:;,=+\\-*/<>\\s]*");
+
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Wymagany jest argument: plik do analizy");
@@ -19,13 +28,43 @@ public class Lexer {
         }
         reader.close();
 
+        keywords.put("and", TokenType.OP_AND);
+        keywords.put("or", TokenType.OP_OR);
+        keywords.put("not", TokenType.OP_NOT);
+        keywords.put("program", TokenType.PROGRAM);
+        keywords.put("procedure", TokenType.PROCEDURE);
+        keywords.put("begin", TokenType.BEGIN);
+        keywords.put("end", TokenType.END);
+        keywords.put("if", TokenType.IF);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("halt", TokenType.HALT);
+        keywords.put("const", TokenType.CONST_VAR);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("until", TokenType.UNTIL);
+        keywords.put("array", TokenType.ARRAY);
+        keywords.put("of", TokenType.OF);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("repeat", TokenType.REPEAT);
+        keywords.put("to", TokenType.TO);
+        keywords.put("do", TokenType.DO);
+        keywords.put("then", TokenType.THEN);
+        keywords.put("integer", TokenType.VARTYPE_INT);
+        keywords.put("real", TokenType.VARTYPE_REAL);
+        keywords.put("boolean", TokenType.VARTYPE_BOOL);
+        keywords.put("true", TokenType.LITERAL_BOOL);
+        keywords.put("false", TokenType.LITERAL_BOOL);
+
         analyze(txt.toString()).forEach(System.out::println);
     }
 
     public static List<Token> analyze(String code){
         ArrayList<Token> tokens = new ArrayList<>();
+
         char c, cc;
         int cst, codelen = code.length()-1;
+        StringBuilder temp;
+        String substr;
+        Matcher matcher;
 
         for (int i = 0; i <= codelen; i++) {
             c = code.charAt(i);
@@ -34,22 +73,42 @@ public class Lexer {
                 case ' ':
                     break;
                 case '{':
-                    StringBuilder comment = new StringBuilder();
+                    temp = new StringBuilder();
                     cst = i;
                     while((cc = code.charAt(++i)) != '}'){
-                        comment.append(cc);
+                        temp.append(cc);
                         if(i == codelen){
-                            System.err.println("Nie zamkniety komentarz! od pozycji" + i);
+                            System.err.println("Nie zamkniety komentarz! od pozycji" + cst);
                             break;
                         }
                     }
-                    tokens.add(new Token(TokenType.COMMENT, comment.toString(), cst));
+                    tokens.add(new Token(TokenType.COMMENT, temp.toString(), cst));
+                    break;
+                case '\'':
+                    temp = new StringBuilder();
+                    cst = i;
+                    while((cc = code.charAt(++i)) != '\''){
+                        temp.append(cc);
+                        if(i == codelen){
+                            System.err.println("Nie zamkniety ciąg znaków! od pozycji" + cst);
+                            break;
+                        }
+                    }
+                    tokens.add(new Token(TokenType.LITERAL_STRING, temp.toString(), cst));
+                    break;
+                case ';':
+                    tokens.add(new Token(TokenType.EXPRESSION_END, ";", i));
                     break;
                 case '.':
+                    if(i != codelen && code.charAt(i+1) == '.') {
+                        tokens.add(new Token(TokenType.RANGE, "..", i++));
+                        break;
+                    }
                 case ',':
                 case '(':
                 case ')':
-                case ';':
+                case '[':
+                case ']':
                     tokens.add(new Token(TokenType.DELIMITER, String.valueOf(c), i));
                     break;
                 case '+':
@@ -61,8 +120,17 @@ public class Lexer {
                 case '-':
                     if(i != codelen && code.charAt(i+1) == '=')
                         tokens.add(new Token(TokenType.OP_SUB_ASSIGN, "-=", i++));
-                    else
-                        tokens.add(new Token(TokenType.OP_SUB, "-", i));
+                    else {
+                        substr = code.substring(i);
+                        if((matcher = LITERAL_REAL_PATTERN.matcher(substr)).find()){
+                            tokens.add(new Token(TokenType.LITERAL_REAL, matcher.group(), i));
+                            i += matcher.end()-1;
+                        }else if ((matcher = LITERAL_INT_PATTERN.matcher(substr)).find()) {
+                            tokens.add(new Token(TokenType.LITERAL_INT, matcher.group(), i));
+                            i += matcher.end()-1;
+                        }else
+                            tokens.add(new Token(TokenType.OP_SUB, "-", i));
+                    }
                     break;
                 case '*':
                     if(i != codelen && code.charAt(i+1) == '=')
@@ -112,8 +180,43 @@ public class Lexer {
                     else
                         tokens.add(new Token(TokenType.DELIMITER, ":", i));
                     break;
+                case '&':
+                    tokens.add(new Token(TokenType.BOP_AND, "&", i));
+                    break;
+                case '|':
+                case '!':
+                    tokens.add(new Token(TokenType.BOP_OR, String.valueOf(c), i));
+                    break;
+                case '~':
+                    tokens.add(new Token(TokenType.BOP_NEG, "~", i));
+                    break;
                 default:
-                    cst = i;
+                    substr = code.substring(i);
+                    boolean hit = false;
+
+                    for(Map.Entry<String, TokenType> e : keywords.entrySet()){
+                        if(substr.startsWith(e.getKey())){
+                            tokens.add(new Token(e.getValue(), e.getKey(), i));
+                            i += e.getKey().length() - 1;
+                            hit = true;
+                            break;
+                        }
+                    }
+
+                    if(!hit){
+                        if((matcher = LITERAL_REAL_PATTERN.matcher(substr)).find()){
+                            tokens.add(new Token(TokenType.LITERAL_REAL, matcher.group(), i));
+                        }else if ((matcher = LITERAL_INT_PATTERN.matcher(substr)).find()) {
+                            tokens.add(new Token(TokenType.LITERAL_INT, matcher.group(), i));
+                        }else if ((matcher = IDENTIFIER_PATTERN.matcher(substr)).find()) {
+                            tokens.add(new Token(TokenType.IDENTIFIER, matcher.group(), i));
+                        } else {
+                            System.err.println("Nie poprawne wyrazenie od pozycji " + i);
+                            return tokens;
+                        }
+
+                        i += matcher.end()-1;
+                    }
                     break;
             }
         }
@@ -122,8 +225,10 @@ public class Lexer {
     }
 
     enum TokenType{
-        LIT_NUMERIC,
-        LIT_STRING,
+        LITERAL_INT(true),
+        LITERAL_REAL(true),
+        LITERAL_STRING(true),
+        LITERAL_BOOL(true),
         OP_ADD,
         OP_SUB,
         OP_MUL,
@@ -149,49 +254,48 @@ public class Lexer {
         BOP_NEG,
         BOP_SHL,
         BOP_SHR,
-        KWRD_PROGRAM,
-        KWRD_PROCEDURE,
-        KWRD_BEGIN,
-        KWRD_END,
-        KWRD_IF,
-        KWRD_ELSE,
-        KWRD_HALT,
-        KWRD_CONST,
-        KWRD_VAR,
-        KWRD_UNTIL,
-        KWRD_ARRAY,
-        KWRD_OF,
-        KRWD_FOR,
-        KWRD_TO,
-        KWRD_DO,
-        KWRD_THEN,
-        VAR_IDENTIFIER,
+        PROGRAM,
+        PROCEDURE,
+        BEGIN,
+        END,
+        IF,
+        ELSE,
+        HALT,
+        CONST_VAR,
+        VAR,
+        UNTIL,
+        ARRAY,
+        OF,
+        FOR,
+        REPEAT,
+        TO,
+        DO,
+        THEN,
         VARTYPE_INT,
         VARTYPE_REAL,
         VARTYPE_BOOL,
-        VARTYPE_CHAR,
-        DELIMITER,
-        COMMENT
+        EXPRESSION_END,
+        RANGE,
+        IDENTIFIER(true),
+        DELIMITER(true),
+        COMMENT(true);
+
+        public final boolean needsIdentifier;
+
+        TokenType(boolean needsIdentifier){
+            this.needsIdentifier = needsIdentifier;
+        }
+
+        TokenType(){
+            this.needsIdentifier = false;
+        }
     }
 
-    static class Token{
-        private final TokenType type;
-        private final String identifier;
-        private final int position;
-
-        public Token(TokenType type, String identifier, int position) {
-            this.type = type;
-            this.identifier = identifier;
-            this.position = position;
-        }
+    record Token(TokenType type, String identifier, int position) {
 
         @Override
-        public String toString() {
-            return "Token{" +
-                    "type=" + type +
-                    ", identifier='" + identifier + '\'' +
-                    ", position=" + position +
-                    '}';
+            public String toString() {
+                return position + ": " + type + (type.needsIdentifier ? " '" + identifier + "'" : "");
+            }
         }
-    }
 }
